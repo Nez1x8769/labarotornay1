822b657402aeeb3de85cb53c89c32b03f91536e9
Nez1x8769 <tolstoy16032006@gmail.com>
2025-10-21 09:18:06 +0300

    Delete 31 задание.py

--- 31 задание.py
+++
@@ -1,8 +0,0 @@
-def get_keys_and_values():
-    name = input("Введите имя: ")
-    age = int(input("Введите возраст: "))
-    city = input("Введите город: ")
-    info_dict = {"name": name, "age": age, "city": city}   
-    print("Ключи:", info_dict.keys())
-    print("Значения:", info_dict.values())
-get_keys_and_values()
